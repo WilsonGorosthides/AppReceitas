3145d8b7fd8177a57c1eed330df653d49a133081
Wilson <netogoro77@gmail.com>
2024-06-21 22:23:24 -0400

    criação das entidades Receita e Usuário

---
+++ app/src/main/java/com/example/appreceitas/dao/ReceitaDao.java
@@ -0,0 +1,39 @@
+package com.example.appreceitas.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.appreceitas.entities.Receita;
+
+import java.util.List;
+
+@Dao
+public interface ReceitaDao {
+
+    // Inserir uma nova receita
+    @Insert
+    void insert(Receita receita);
+
+    // Atualizar uma receita existente
+    @Update
+    void update(Receita receita);
+
+    // Deletar uma receita
+    @Delete
+    void delete(Receita receita);
+
+    // Deletar todas as receitas
+    @Query("DELETE FROM receitas")
+    void deleteAllReceitas();
+
+    // Consultar todas as receitas
+    @Query("SELECT * FROM receitas")
+    List<Receita> getAllReceitas();
+
+    // Consultar receitas por título
+    @Query("SELECT * FROM receitas WHERE titulo LIKE :titulo")
+    List<Receita> getReceitasByTitle(String titulo);
+}
